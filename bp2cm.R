440f0c829ba6535d32498cf9119ee0c94a8165dd
John T. Lovell <johntlovell@gmail.com>
2015-06-21 11:17:54 -0500

    added estimation of conf. ints.

--- bp2cm.R
+++ bp2cm.R
@@ -1,4 +1,4 @@
-bp2cm<-function(cross,geneID=gff$A.id,chrID=gff$chr, bpPos=gff$start, markerBP=NULL, markerID=NULL, splitByCentromere=TRUE){
+bp2cm<-function(cross,geneID=gff$A.id,chrID=gff$chr, bpPos=gff$start, markerBP=NULL, markerID=NULL, splitByCentromere=TRUE,minCI=1){
   #create an annotation file with the necessary columns
   if(!is.null(markerBP) & !is.null(markerID)){
     gff<-data.frame(geneID,markerID=NULL,chrID,bpPos)
@@ -31,18 +31,27 @@ bp2cm<-function(cross,geneID=gff$A.id,chrID=gff$chr, bpPos=gff$start, markerBP=N
       g<-g[g$chrID==bestChr,]
     }
     g<-g[order(g$bpPos),]
-    plot(g$bpPos,g$pos, main=paste("mapping vs. physical position of markers on chr",i), cex=.2, pch=19, ylab="mapping position (cM)", xlab="physical postion (bp)")
+    #plot(g$bpPos,g$pos, main=paste("mapping vs. physical position of markers on chr",i), cex=.2, pch=19, ylab="mapping position (cM)", xlab="physical postion (bp)")
     if(splitByCentromere){
       diffs<-diff(g$bpPos)
       ot<-outlierTest(lm(diff(g$bpPos)~diff(g$pos)),cutoff=Inf, n.max=10)
       cmIndex<-as.numeric(names(ot[[2]][ot[[2]]<0.1]))
       centromere<-g$pos[c(min(cmIndex),(max(cmIndex)+1))]
-      rect(ybottom=centromere[1], ytop=centromere[2],xleft=0,xright=max(g$bpPos), col=rgb(1,0,0,.2))
+      #rect(ybottom=centromere[1], ytop=centromere[2],xleft=0,xright=max(g$bpPos), col=rgb(1,0,0,.2))
+      centromere.range<-range(centromere)
+      if(min(centromere)<5){
+        centromere.range[1]<-0
+        arms<-2:3
+      }else{
+        arms<-1:3
+      }
       splits<-list(top=c(0,(min(centromere))),
-                   centromere=range(centromere),
+                   centromere=centromere.range,
                    bottom=c((max(centromere)), max(g$pos)))
       out.all<-data.frame()
-      for(j in 1:3){
+      par(mfrow=c(3,1))
+      
+      for(j in arms){
         g1<-g[g$pos>=splits[[j]][1] & g$pos<=splits[[j]][2],]
         bps<-c(min(g1$bpPos), max(g1$bpPos))
         if(j==2){
@@ -52,25 +61,62 @@ bp2cm<-function(cross,geneID=gff$A.id,chrID=gff$chr, bpPos=gff$start, markerBP=N
         }        
         geneID<-gff1$geneID
         bpPos<-data.frame(bpPos=gff1$bpPos)
-        if(nrow(g1)<4){
-          mod<-with(g1, lm(pos~bpPos))
-          abline(mod)
-          out<-data.frame(predict(mod,bpPos),bpPos,geneID)
-          colnames(out)[1]<-"imputed.cm"
-        }else{
-          mod<-smooth.spline(g1$bpPos,g1$pos, df=sqrt(nrow(g1)))
-          lines(mod)
-          out<-data.frame(predict(mod,x=bpPos),geneID)
-          colnames(out)[1:2]<-c("bpPos","imputed.cm")
-        }
+        df1<-ifelse(floor(nrow(g1)/6)==0,1,floor(nrow(g1)/6))
+        mod <- lm(pos ~ ns(bpPos, df=df1) , data=g1)
+        plot(g1$bpPos,g1$pos)
+        pred<-predict(mod, newdata=data.frame(bpPos=gff1$bpPos), interval = "prediction", level=.95)
+        pred<-cbind(data.frame(bpPos=gff1$bpPos), pred)
+        colnames(pred)[2]<-"pos"
+        out<-pred[order(pred$bpPos),]
+        for(k in c("pos","lwr","upr")) lines(out[,"bpPos"],out[,k], col="red")
+        colnames(out)[1:2]<-c("bpPos","imputed.cm")
+        out$geneID<-geneID
+        
         max.marker<-max(g1$pos)
         min.marker<-min(g1$pos)
         out$imputed.cm[out$imputed.cm>max.marker]<-max.marker
         out$imputed.cm[out$imputed.cm<min.marker]<-min.marker
-        out.dat<-merge(gff1,out, by=c("geneID","bpPos"))
-        out.all<-rbind(out.all,out.dat) 
+        out$lwr[out$lwr<0]<-0
+        if(j==3){
+          out$upr[out$upr>max(g1$pos)]<-max(g1$pos)
+        }
+        out$arm<-ifelse(j==1,"proximate",ifelse(j==2, "centromere", "distal"))
+        out$chromosome<-i
+        out.all<-rbind(out.all,out) 
       }
-      with(out.all, plot(bpPos,imputed.cm, main=paste("imputed positions for each gene on chr", i), cex=.2, pch=19))
+      par(mfrow=c(1,1))
+      plot(g$bpPos,g$pos, main=paste("mapping vs. physical position of markers on chr",i), cex=.2, pch=19, ylab="mapping position (cM)", xlab="physical postion (bp)")
+      if(min(centromere)<5){
+        plwr.max=0
+        pupr.max=min(out.all$upr[out.all$arm=="distal"])
+        p.max=0
+      }else{
+        pupr.max<-max(out.all$upr[out.all$arm=="proximate"])
+        plwr.max<-max(out.all$lwr[out.all$arm=="proximate"])
+        p.max<-max(out.all$bpPos[out.all$arm=="proximate"])
+      }
+
+      qupr.min<-min(out.all$upr[out.all$arm=="distal"])
+      qlwr.min<-min(out.all$lwr[out.all$arm=="distal"])
+      q.min<-min(out.all$bpPos[out.all$arm=="distal"])
+      lwr<-data.frame(lwr=c(plwr.max,qlwr.min),bp=c(p.max,q.min))
+      upr<-data.frame(upr=c(pupr.max,qupr.min),bp=c(p.max,q.min))
+      
+      upr.mod<-lm(upr~bp, data=upr)
+      lwr.mod<-lm(lwr~bp, data=lwr)
+      upr.pred<-predict(upr.mod, data.frame(bp=out.all$bpPos[out.all$arm=="centromere"]))
+      lwr.pred<-predict(lwr.mod, data.frame(bp=out.all$bpPos[out.all$arm=="centromere"]))
+      out.all$upr[out.all$arm=="centromere"]<-upr.pred
+      out.all$lwr[out.all$arm=="centromere"]<-lwr.pred
+      if(!is.na(minCI)){
+        out.all$upr[out.all$upr-out.all$imputed.cm<1]<-out.all$imputed.cm[out.all$upr-out.all$imputed.cm<1]+minCI
+        out.all$lwr[out.all$imputed.cm-out.all$lwr<1]<-out.all$imputed.cm[out.all$imputed.cm-out.all$lwr<1]-minCI
+      }
+      out.all<-out.all[order(out.all$bpPos),]
+      #with(out.all, plot(bpPos,imputed.cm, type="p", cex=.1))
+      with(out.all, lines(bpPos,lwr, type="l",col="blue"))
+      with(out.all, lines(bpPos,upr, type="l",col="red"))
+      #with(out.all, plot(bpPos,imputed.cm, main=paste("imputed positions for each gene on chr", i), cex=.2, pch=19))
     }else{
       g1<-g
       gff1<-gff[gff$chr==bestChr,]
